a5ec2c17d8c4a806618c11d1ce902a1af91b35d9
Mrpongalfer <pongalfer@gmail.com>
2024-10-29 20:40:33 -0500

    universal_deploy.ps1

--- universal_deploy.ps1
+++ universal_deploy.ps1
@@ -1,1 +1,1 @@
-iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Mrpongalfer/omnitidenexus/main/universal_deploy.ps1'))
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Mrpongalfer/omnitidenexus/main/universal_deploy.ps1" -OutFile universal_deploy.ps1; ./universal_deploy.ps1
